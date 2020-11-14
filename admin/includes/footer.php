5154d4e13b1b8deb74812fb42f8d1f439d8a6254
k_addae <iamkarikari98@gmail.com>
2020-11-14 15:50:31 +0000

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong>  Brought To You By <a href="https://www.noidatut.com">Dr Tutorial NoidaTut</a>Ref* Code Project </strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> Brought To You By<a href="https://www.noidatut.com"> Dr Tutorial NoidaTut</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
